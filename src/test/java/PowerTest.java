b87b574fa1403a4c1416c61030f25ebab199096d
Sanskar <Sanskartayal2@gmail.com>
2024-04-15 23:51:21 +0530

    Completed The Solution

---
+++ src/test/java/PowerTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import powerpackage.PowerFinder;
+
+public class PowerTest {
+    @Test
+    public void oneRaisedToOneIsOne()
+    {
+        assert PowerFinder.of(1,1) == 1;
+    }
+
+    @Test
+    public void twoRaisedToOneIsTwo()
+    {
+        assert PowerFinder.of(2,1) == 2;
+    }
+
+    @Test
+    public void twoRaisedToPowerTwoIsFour()
+    {
+        assert PowerFinder.of(2, 2) == 4;
+    }
+
+    @Test
+    public void twoRaisedToPowerThreeIsSix()
+    {
+        assert PowerFinder.of(3, 2) == 3*3;
+    }
+}
